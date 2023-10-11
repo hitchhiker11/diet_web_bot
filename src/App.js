423647e251bb73ce22732c1b11c58cf3c3f63ad4
Alexandr Bormotov <96291048+hitchhiker11@users.noreply.github.com>
2023-10-11 19:12:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [stage, setStage] = useState(1);
+  const [showAdditionalFields, setShowAdditionalFields] = useState(false);
   const [userData, setUserData] = useState({
     name: "",
     age: "",
@@ -17,46 +17,82 @@ function App() {
 
   const handleChange = e => {
     const { name, value } = e.target;
-    setUserData({ ...userData, [name]: value });
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    if (stage === 1 && !userData.metabolism) {
-      setStage(2);
-    } else {
-      // Здесь вы можете обработать и отправить данные на сервер
-      console.log(userData);
-    }
+    // здесь код для отправки данных на сервер
+    console.log(userData);
   };
-
-  useEffect(() => {
-    // Место для функций или эффектов, которые нужно выполнить при изменении stage
-  }, [stage]);
 
   return (
     <div className="App">
       <h1>Diet App</h1>
       <form onSubmit={handleSubmit}>
-        {/* ... другие поля ввода ... */}
-        {stage === 1 ? (
+        <label>
+          ФИО:
+          <input type="text" name="name" value={userData.name} onChange={handleChange} />
+        </label>
+        <label>
+          Возраст:
+          <input type="number" name="age" value={userData.age} onChange={handleChange} />
+        </label>
+        <label>
+          Пол:
+          <select name="gender" value={userData.gender} onChange={handleChange}>
+            <option value="">Выбрать...</option>
+            <option value="male">Мужчина</option>
+            <option value="female">Женщина</option>
+          </select>
+        </label>
+        <label>
+          Уровень активности:
+          <select name="activity" value={userData.activity} onChange={handleChange}>
+            <option value="">Выбрать...</option>
+            <option value="low">Низкий</option>
+            <option value="medium">Средний</option>
+            <option value="high">Высокий</option>
+          </select>
+        </label>
+        <label>
+          Метаболизм:
+          <input 
+            type="number" 
+            name="metabolism" 
+            value={userData.metabolism} 
+            onChange={handleChange} 
+            disabled={showAdditionalFields}
+          />
+        </label>
+        {!userData.metabolism && 
+          <button type="button" onClick={() => setShowAdditionalFields(true)}>
+            Не знаю свой метаболизм
+          </button>
+        }
+        {showAdditionalFields && (
           <>
             <label>
-              Метаболизм:
-              <input type="number" name="metabolism" value={userData.metabolism} onChange={handleChange} />
+              Рост:
+              <input type="number" name="height" value={userData.height} onChange={handleChange} />
             </label>
-            {!userData.metabolism && (
-              <button type="button" onClick={() => setStage(2)}>Не знаю метаболизм</button>
-            )}
-          </>
-        ) : (
-          <>
-            {/* Поля для ввода роста, веса и окружности, если metabolism неизвестен */}
-            {/* ... */}
-            <button type="button" onClick={() => setStage(1)}>Вернуться назад</button>
+            <label>
+              Вес:
+              <input type="number" name="weight" value={userData.weight} onChange={handleChange} />
+            </label>
+            <label>
+              Окружность бедра:
+              <input type="number" name="hipCircumference" value={userData.hipCircumference} onChange={handleChange} />
+            </label>
+            <label>
+              Окружность запястья:
+              <input type="number" name="wristCircumference" value={userData.wristCircumference} onChange={handleChange} />
+            </label>
           </>
         )}
-        {/* ... кнопки и другие элементы интерфейса ... */}
         <button type="submit">Подтвердить</button>
       </form>
     </div>
