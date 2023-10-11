9d5ab4183d524c02f26ba351d6d4d50712446ae5
Alexandr Bormotov <96291048+hitchhiker11@users.noreply.github.com>
2023-10-11 19:18:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,74 +25,69 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    // здесь код для отправки данных на сервер
     console.log(userData);
   };
 
   return (
     <div className="App">
-      <h1>Ассистент-диетоло</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          ФИО:
-          <input type="text" name="name" value={userData.name} onChange={handleChange} />
-        </label>
-        <label>
-          Возраст:
-          <input type="number" name="age" value={userData.age} onChange={handleChange} />
-        </label>
-        <label>
-          Пол:
-          <select name="gender" value={userData.gender} onChange={handleChange}>
-            <option value="">Выбрать...</option>
-            <option value="male">Мужчина</option>
-            <option value="female">Женщина</option>
-          </select>
-        </label>
-        <label>
-          Уровень активности:
-          <select name="activity" value={userData.activity} onChange={handleChange}>
-            <option value="">Выбрать...</option>
-            <option value="low">Низкий</option>
-            <option value="medium">Средний</option>
-            <option value="high">Высокий</option>
-          </select>
-        </label>
-        <label>
-          Метаболизм:
-          <input 
-            type="number" 
-            name="metabolism" 
-            value={userData.metabolism} 
-            onChange={handleChange} 
-            disabled={showAdditionalFields}
-          />
-        </label>
-        {!userData.metabolism && 
-          <button type="button" onClick={() => setShowAdditionalFields(true)}>
+      <h1>Diet App</h1>
+      <form onSubmit={handleSubmit} style={{textAlign: 'left', marginLeft: '40px'}}>
+        <div style={{maxWidth: '300px'}}>
+          <label>
+            ФИО:
+            <input type="text" name="name" value={userData.name} onChange={handleChange} style={{width: '100%'}} />
+          </label>
+          <label>
+            Возраст:
+            <input type="number" name="age" value={userData.age} onChange={handleChange} style={{width: '100%'}} />
+          </label>
+          <label>
+            Пол:
+            <select name="gender" value={userData.gender} onChange={handleChange} style={{width: '100%'}}>
+              <option value="">Выбрать...</option>
+              <option value="male">Мужчина</option>
+              <option value="female">Женщина</option>
+            </select>
+          </label>
+          <label>
+            Уровень активности:
+            <select name="activity" value={userData.activity} onChange={handleChange} style={{width: '100%'}}>
+              <option value="">Выбрать...</option>
+              <option value="low">Низкий</option>
+              <option value="medium">Средний</option>
+              <option value="high">Высокий</option>
+            </select>
+          </label>
+          {!showAdditionalFields && (
+            <label>
+              Метаболизм:
+              <input type="number" name="metabolism" value={userData.metabolism} onChange={handleChange} style={{width: '100%'}} />
+            </label>
+          )}
+          <button type="button" onClick={() => setShowAdditionalFields(!showAdditionalFields)}>
             Не знаю свой метаболизм
           </button>
-        }
-        {showAdditionalFields && (
-          <>
-            <label>
-              Рост:
-              <input type="number" name="height" value={userData.height} onChange={handleChange} />
-            </label>
-            <label>
-              Вес:
-              <input type="number" name="weight" value={userData.weight} onChange={handleChange} />
-            </label>
-            <label>
-              Окружность бедра:
-              <input type="number" name="hipCircumference" value={userData.hipCircumference} onChange={handleChange} />
-            </label>
-            <label>
-              Окружность запястья:
-              <input type="number" name="wristCircumference" value={userData.wristCircumference} onChange={handleChange} />
-            </label>
-          </>
-        )}
+          {showAdditionalFields && (
+            <>
+              <label>
+                Рост:
+                <input type="number" name="height" value={userData.height} onChange={handleChange} style={{width: '100%'}} />
+              </label>
+              <label>
+                Вес:
+                <input type="number" name="weight" value={userData.weight} onChange={handleChange} style={{width: '100%'}} />
+              </label>
+              <label>
+                Окружность бедра:
+                <input type="number" name="hipCircumference" value={userData.hipCircumference} onChange={handleChange} style={{width: '100%'}} />
+              </label>
+              <label>
+                Окружность запястья:
+                <input type="number" name="wristCircumference" value={userData.wristCircumference} onChange={handleChange} style={{width: '100%'}} />
+              </label>
+            </>
+          )}
+        </div>
         <button type="submit">Подтвердить</button>
       </form>
     </div>
