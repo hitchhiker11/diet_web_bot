17b74e666f86146475677cac9fb8201a386b93c0
Alexandr Bormotov <96291048+hitchhiker11@users.noreply.github.com>
2023-10-21 22:29:24 +0300

    Update MainScreen.js

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 // import './MainScreen.css';
-
+import './App.css';
 function MainScreen({ onOpenMetabolism }) {
   return (
     <div className="MainScreen">
