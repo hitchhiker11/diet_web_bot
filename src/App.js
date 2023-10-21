a9044a951d979f09bb07eb6868b86ba1f781da38
Alexandr Bormotov <96291048+hitchhiker11@users.noreply.github.com>
2023-10-21 18:30:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-
-
 import React, { useState } from 'react';
 import './App.css';
+
+
+function calculateMetabolism(data) {
+  let BM, PM1, PM2, IM;
+
+  // Расчет базового метаболизма BM
+  if (data.BM) {
+    BM = data.BM;
+  } else {
+    if (data.gender === "M") {
+      BM = 10 * data.weight + 6.25 * data.height - 5 * data.age + 5;
+    } else {
+      BM = 10 * data.weight + 6.25 * data.height - 5 * data.age - 161;
+    }
+  }
+
+  // Расчетный метаболизм 1 PM1
+  if (BM <= 1400) {
+    PM1 = 1400;
+  } else if (BM > 1400 && BM <= 1500) {
+    PM1 = BM - 300;
+  } else {
+    PM1 = 1200;
+  }
+
+  // Расчетный метаболизм 2 PM2 в зависимости от уровня физической активности
+  switch (data.activityLevel) {
+    case "H":
+      PM2 = PM1;
+      break;
+    case "C":
+      PM2 = PM1 + 300;
+      break;
+    case "B":
+      PM2 = PM1 + 500;
+      break;
+    default:
+      PM2 = PM1;
+  }
+
+  // Итоговый метаболизм IM в зависимости от цели
+  if (data.goal === "lose") {
+    IM = PM2;
+  } else if (data.goal === "maintain") {
+    IM = PM2 + 300;
+  } else {
+    IM = PM2;
+  }
+
+  return IM;  // Возвращает итоговый метаболизм
+}
 
 function App() {
   const [showAdditionalFields, setShowAdditionalFields] = useState(false);
@@ -47,11 +96,26 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    const initialMetabolism = calculateMetabolism();
-    setCalculatedMetabolism(initialMetabolism);
-    const finalValue = calculatePM2();
-    setFinalMetabolism(finalValue);
+
+    // Достаем нужные данные из состояния
+    const { gender, weight, height, age, activity, name, metabolism } = userData;
+
+    // Проверяем, если известен метаболизм, используем его, иначе используем 0
+    const knownBM = metabolism ? parseFloat(metabolism) : 0;
+
+    const finalMetabolism = calculateMetabolism({
+      BM: knownBM,
+      gender: gender,
+      weight: parseFloat(weight),
+      height: parseFloat(height),
+      age: parseFloat(age),
+      activityLevel: activity,
+      goal: "lose"  // По умолчанию ставим "lose", так как у вас не было выбора для этой опции в форме
+    });
+
+    setFinalMetabolism(finalMetabolism);
   };
+
 
   return (
     <div className="App">
@@ -121,3 +185,4 @@ function App() {
 }
 
 export default App;
+
