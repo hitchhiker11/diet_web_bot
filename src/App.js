0a5a5c740f5b3865c8cb238a190d0398f584fbe2
Alexandr Bormotov <96291048+hitchhiker11@users.noreply.github.com>
2023-12-01 15:21:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { useState } from 'react';
 import MainScreen from './MainScreen';
 import MetabolismScreen from './MetabolismScreen';
+import ProductSelection from './ProductSelection';
+import DietManagement from './DietManagement';
+import AddProductToDiet from './AddProductToDiet';
 import './App.css';
+
 function App() {
   const [screen, setScreen] = useState('main');
+  const [diet, setDiet] = useState([]); // Добавляем состояние для управления рационом
 
   const openMetabolismScreen = () => {
     setScreen('metabolism');
   };
 
-  const goBackToMainScreen = () => {
-    setScreen('main');
+  const openProductSelection = () => {
+    setScreen('productSelection');
+  };
+
+  const openDietManagement = () => {
+    setScreen('dietManagement');
+  };
+
+  const addProductToDiet = (product) => {
+    setDiet([...diet, product]);
   };
 
   return (
     <div>
       {screen === 'main' && <MainScreen onOpenMetabolism={openMetabolismScreen} />}
       {screen === 'metabolism' && <MetabolismScreen />}
-      {screen !== 'main' && <button onClick={goBackToMainScreen}>Вернуться на главный экран</button>}
+      {screen === 'productSelection' && <ProductSelection />}
+      {screen === 'dietManagement' && <DietManagement diet={diet} />}
+      {screen === 'addProduct' && <AddProductToDiet onAdd={addProductToDiet} />}
+
+      {screen !== 'main' && (
+        <button onClick={() => setScreen('main')}>Вернуться на главный экран</button>
+      )}
+
+      {/* Кнопки для навигации */}
+      <button onClick={openProductSelection}>Выбор Продуктов</button>
+      <button onClick={openDietManagement}>Управление Рационом</button>
+      <button onClick={() => setScreen('addProduct')}>Добавить Продукт в Рацион</button>
     </div>
   );
 }
-
 
 export default App;
